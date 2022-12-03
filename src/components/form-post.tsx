56f34fd106a03ef235ed7adfa16952b697004b5c
Itor Isaias <itor.silva@5by5.com.br>
2022-12-02 23:50:06 -0300

    feat: 🎸 install fonts and react-hook-form

---
+++ src/components/form-post.tsx
@@ -0,0 +1,156 @@
+import { SubmitHandler, useForm } from "react-hook-form";
+
+export type FormProps = {
+  onSubmit: SubmitHandler<FormFieldValues>;
+};
+export type FormFieldValues = {
+  title: string;
+  content: string;
+  files: FileList[];
+}
+
+export default function FormPost({ onSubmit }: FormProps) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormFieldValues>();
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="shadow sm:overflow-hidden sm:rounded-md">
+        <div className="space-y-6 bg-white px-4 py-5 sm:p-6">
+          <div>
+            <label
+              htmlFor="title"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Title
+            </label>
+            <div className="mt-1">
+              <input
+                type="text"
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm text-gray-800"
+                placeholder="My good day..."
+                {...register("title", { required: true, maxLength: 50 })}
+              />
+            </div>
+          </div>
+
+          <div>
+            <label
+              htmlFor="title"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Content
+            </label>
+
+            <div className="mt-1">
+              <textarea
+                rows={3}
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm text-gray-800"
+                placeholder="Describe your..."
+                {...register("content")}
+              ></textarea>
+            </div>
+            <p className="mt-2 text-sm text-gray-500">
+              Brief description for your profile. URLs are hyperlinked.
+            </p>
+          </div>
+
+          {/* <div>
+                <label className="block text-sm font-medium text-gray-700">
+                  Photo
+                </label>
+                <div className="mt-1 flex items-center">
+                  <span className="inline-block h-12 w-12 overflow-hidden rounded-full bg-gray-100">
+                    <svg
+                      className="h-full w-full text-gray-300"
+                      fill="currentColor"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+                    </svg>
+                  </span>
+                  <button
+                    type="button"
+                    className="ml-5 rounded-md border border-gray-300 bg-white py-2 px-3 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                  >
+                    Change
+                  </button>
+                </div>
+              </div> */}
+
+          <div>
+            <label className="block text-sm font-medium text-gray-700">
+              Cover photo
+            </label>
+            <div className="mt-1 flex justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6">
+              <div className="space-y-1 text-center">
+                <svg
+                  className="mx-auto h-12 w-12 text-gray-400"
+                  stroke="currentColor"
+                  fill="none"
+                  viewBox="0 0 48 48"
+                  aria-hidden="true"
+                >
+                  <path
+                    d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+                <div className="flex text-sm text-gray-600">
+                  <label
+                    htmlFor="files"
+                    className="relative cursor-pointer rounded-md bg-white font-medium text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500"
+                  >
+                    <span>Upload a file</span>
+                    <input type="file" className="sr-only" name="files" />
+                  </label>
+                  <p className="pl-1">or drag and drop</p>
+                </div>
+                <p className="text-xs text-gray-500">
+                  PNG, JPG, GIF up to 10MB
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <input type="file" id="files" {...register('files')} />
+
+          {/* <div>
+            <div className="flex items-start">
+              <div className="flex h-5 items-center">
+                <input
+                  id="comments"
+                  name="comments"
+                  type="checkbox"
+                  className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                />
+              </div>
+              <div className="ml-3 text-sm">
+                <label htmlFor="comments" className="font-medium text-gray-700">
+                  Comments
+                </label>
+                <p className="text-gray-500">
+                  Get notified when someones posts a comment on a posting.
+                </p>
+              </div>
+            </div>
+          </div> */}
+        </div>
+
+        <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
+          <button
+            type="submit"
+            className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+          >
+            Save
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+}
