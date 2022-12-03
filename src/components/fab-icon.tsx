14d3da1e9283ada81baabea4dd41458a17dcfa5e
Itor Isaias <itor.isaias@gmail.com>
2022-12-03 20:17:28 -0300

    feat: 🎸 create fab icon

---
+++ src/components/fab-icon.tsx
@@ -0,0 +1,28 @@
+export type FabiconProps = {
+  onClick?: () => void;
+};
+
+export default function FabIcon({ onClick }: FabiconProps) {
+  return (
+    <button
+      type="button"
+      className="fixed z-90 bottom-10 right-8 bg-gray-800 w-16 h-16 rounded-full drop-shadow-lg flex justify-center items-center text-white text-4xl hover:bg-gray-900 hover:drop-shadow-2xl"
+      onClick={onClick}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        className="w-11 h-11"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M12 4.5v15m7.5-7.5h-15"
+        />
+      </svg>
+    </button>
+  );
+}
