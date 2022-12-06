76d4e369ef7d46edfe32ebe922abd78958542930
Itor Isaias <itor.silva@5by5.com.br>
2022-12-06 17:03:14 -0300

    feat: 🎸 create routers post and user

--- src/server/routers/post.ts
+++ src/server/routers/post.ts
@@ -2,7 +2,79 @@ import { router, protectedProcedure, publicProcedure } from "../trpc";
 import { z } from "zod";
 
 export const postRouter = router({
-  create: publicProcedure
+  list: publicProcedure
+    .input(
+      z
+        .object({
+          orderBy: z.array(
+            z.object({
+              sort_by: z.enum(["asc", "desc"]).default('asc'),
+              order_by: z.enum([
+                "created_at",
+                "published_at",
+                "updated_at",
+                "views",
+              ]),
+            })
+          ),
+        })
+        .default({
+          orderBy: [
+            {
+              sort_by: 'desc',
+              order_by: 'published_at'
+            }
+          ],
+        })
+    )
+    .query(({ ctx, input }) => {
+      return ctx.prisma.post.findMany({
+        include: {
+          author: true,
+        },
+        orderBy: input.orderBy.map((item) => ({
+          [item.order_by]: item.sort_by,
+        })),
+        where: {
+          deleted_at: null,
+          published_at: {
+            not: null,
+          },
+        },
+      });
+    }),
+  detail: publicProcedure
+    .input(
+      z.object({
+        post_id: z.number(),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const [postDetail] = await Promise.all([
+        ctx.prisma.post.findFirst({
+          where: {
+            id: input.post_id,
+          },
+          include: {
+            author: true,
+            tags: true,
+          },
+        }),
+        ctx.prisma.post.update({
+          data: {
+            views: {
+              increment: 1,
+            },
+          },
+          where: {
+            id: input.post_id,
+          },
+        }),
+      ]);
+
+      return postDetail;
+    }),
+  create: protectedProcedure
     .input(
       z.object({
         title: z.string(),
@@ -24,11 +96,59 @@ export const postRouter = router({
 
       return newPost;
     }),
-  list: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany({
-      include: {
-        author: true
-      }
-    });
-  }),
+  update: protectedProcedure
+    .input(
+      z.object({
+        title: z.string(),
+        content: z.string(),
+        post_id: z.number(),
+      })
+    )
+    .mutation(async ({ input, ctx }) => {
+      const postUpdated = await ctx.prisma.post.update({
+        data: {
+          title: input.title,
+          content: input.content,
+        },
+        where: {
+          id: input.post_id,
+        },
+      });
+
+      return postUpdated;
+    }),
+  delete: protectedProcedure
+    .input(
+      z.object({
+        post_id: z.number(),
+      })
+    )
+    .mutation(async ({ input, ctx }) => {
+      await ctx.prisma.post.update({
+        data: {
+          deleted_at: new Date(),
+        },
+        where: {
+          id: input.post_id,
+        },
+      });
+    }),
+  publish: protectedProcedure
+    .input(
+      z.object({
+        post_id: z.number(),
+      })
+    )
+    .mutation(async ({ input, ctx }) => {
+      const postUpdated = await ctx.prisma.post.update({
+        data: {
+          published_at: new Date(),
+        },
+        where: {
+          id: input.post_id,
+        },
+      });
+
+      return postUpdated;
+    }),
 });
--- src/server/routers/user.ts
+++ src/server/routers/user.ts
@@ -1,8 +1,42 @@
-import { router, publicProcedure } from "../trpc";
 import { z } from "zod";
+import { router, protectedProcedure, publicProcedure } from "../trpc";
 
 export const userRouter = router({
-  list: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.user.findMany();
+  me: protectedProcedure.query(({ ctx }) => {
+    return ctx.prisma.user.findFirst({
+      include: {
+        _count: {
+          select: {
+            posts: true,
+          },
+        },
+        posts: true
+      },
+      where: {
+        email: ctx.session.user?.email!,
+      },
+    });
   }),
+  publicProfile: publicProcedure
+    .input(z.object({ user_id: z.number() }))
+    .query(({ ctx, input }) => {
+      return ctx.prisma.user.findFirst({
+        include: {
+          posts: {
+            take: 10,
+            where: {
+              published_at: {
+                not: null,
+              },
+            },
+            orderBy: {
+              created_at: 'desc'
+            }
+          },
+        },
+        where: {
+          id: input.user_id,
+        },
+      });
+    }),
 });
