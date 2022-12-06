77ee2acb4825092a38b5b6e20b24ce626a9d2d8c
Itor Isaias <itor.silva@5by5.com.br>
2022-12-06 19:39:34 -0300

    feat: 🎸 install and setup analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from "next-auth/react"
-import { trpc } from '../utils/trpc';
-import Header from '../components/header';
+import { useAnalytics } from '@happykit/analytics';
+
+import { trpc } from 'utils/trpc';
+import Header from 'components/header';
 
 function App({ Component, pageProps: { session, ...pageProps }, }: AppProps) {
+  useAnalytics({
+    publicKey: process.env.NEXT_PUBLIC_ANALYTICS_ENV_KEY!,
+    skipHostnames: [],
+  })
+
   return (
     <SessionProvider session={session}>
       <Header />
