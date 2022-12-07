5d4268572177e4ebd1815c639c11f7c547af7c35
Itor Isaias <itor.isaias@gmail.com>
2022-12-06 22:23:52 -0300

    refactor: 💡 remove unused routerrr

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -1,24 +1,10 @@
-import { z } from 'zod';
-import { publicProcedure, router } from '../trpc';
-
-import { userRouter } from './user';
+import { router } from 'server/trpc';
 import { postRouter } from './post';
+import { userRouter } from './user';
 
 export const appRouter = router({
-  user: userRouter, // put procedures under "user" namespace
-  post: postRouter, // put procedures under "post" namespace
-  hello: publicProcedure
-    .input(
-      z.object({
-        name: z.string(),
-      }),
-    )
-    .query(({ input }) => {
-      return {
-        msg: `hello ${input.name}`,
-      };
-    }),
+  user: userRouter,
+  post: postRouter,
 });
 
-// export type definition of API
 export type AppRouter = typeof appRouter;
