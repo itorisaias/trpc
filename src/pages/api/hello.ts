61737b2259082bbb3d0acb24fc25b6f761e51b1f
Itor Isaias <itor.silva@5by5.com.br>
2023-01-05 16:24:35 -0300

    update logger ip

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,6 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+import requestIp from 'request-ip'
+
 type Data = {
   name: string
 }
@@ -9,6 +11,14 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  const detectedIp = requestIp.getClientIp(req)
+  const forwarded = req.headers["x-forwarded-for"]
   console.log('my request')
+  console.log(`forwarded ${forwarded}`)
+  console.log(`detectedIp ${detectedIp}`)
+  console.log(`X-Forwarded-For: ${req.headers['X-Forwarded-For']}`)
+  console.log(`CF-Connecting-IP: ${req.headers['CF-Connecting-IP']}`)
+  console.log(`True-Client-IP: ${req.headers['True-Client-IP']}`)
+
   res.status(200).json({ name: 'John Doe' })
 }
